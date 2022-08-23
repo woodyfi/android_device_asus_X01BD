8301a48b1c001a9326915008f985ca14ebb9e14d
khirendra03 <khirendrapatel3@gmail.com>
2022-08-24 01:05:19 +0530

    X01BD: [Temp] allow selinux neverallow
    Signed-off-by: khirendra03 <khirendrapatel3@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,3 +27,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
 # Inherit the proprietary files
 include vendor/asus/X01BD/BoardConfigVendor.mk
+
+# selinux neverallow
+SELINUX_IGNORE_NEVERALLOWS := true
