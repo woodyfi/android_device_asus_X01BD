3f43bc588f80e2b8f11b68123abdd0abd7ff4f74
ELbee <85691313+woodyfi@users.noreply.github.com>
2024-02-23 16:03:09 +0700

    Update afterlife_X01BD.mk

--- afterlife_X01BD.mk
+++ afterlife_X01BD.mk
@@ -10,22 +10,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common VoltageOS stuff
-$(call inherit-product, vendor/voltag/config/common_full_phone.mk)
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Bootanimation
-TARGET_BOOT_ANIMATION_RES := 2160
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
-# Voltage Build Type
-VOLTAGE_BUILD_TYPE := OFFICIAL
+# Afterlife Build Type
+AFTERLIFE_BUILD_TYPE := UNOFFICIAL
+AFTERLIFE_MAINTAINER := PARDO OYO
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := voltage_X01BD
+PRODUCT_NAME := afterlife_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Asus Zenfone Max Pro M2
